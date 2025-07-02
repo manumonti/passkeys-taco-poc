3d99598b290c2637a8b825fbabd3560a54056654
Manuel Montenegro <manuel@nucypher.com>
2025-07-02 17:24:25 +0200

    Add UI buttons and images

---
+++ app/components/register-passkey.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+function handleClick() {
+  console.log("Registry new Passkey");
+}
+
+export default function RegisterPasskey() {
+  return (
+    <button
+      className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+      onClick={handleClick}
+    >
+      Registry new Passkey
+    </button>
+  );
+}
